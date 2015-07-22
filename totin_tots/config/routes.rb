75ae00206844f5478e726d2238cbc4c5ef8f4a76
John M. Bier <johnmbier@gmail.com>
2015-07-22 10:25:46 -0400

    users controller created

--- totin_tots/config/routes.rb
+++ totin_tots/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+  get 'users/index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get 'users/new'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'users/create'
+
+  get 'users/show'
+
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
